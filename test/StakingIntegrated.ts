a5585e2ab62abd9331f2c2b6338bc48fcd1ade9f
juuroudojo <105825252+juuroudojo@users.noreply.github.com>
2023-03-14 19:34:13 +0200

    Fix: cleanup

--- test/StakingIntegrated.ts
+++ test/StakingIntegrated.ts
@@ -4,6 +4,11 @@ import { Signer, BigNumber } from 'ethers';
 import { SimpleStakingHandler__factory, SimpleAirdropHandler__factory, SimpleAirdropHandler, SimpleStakingHandler, RukiaToken__factory, RukiaToken, CaminoHub__factory, CaminoHub} from '../typechain-types';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
+async function skipDays(days: number) {
+  ethers.provider.send("evm_increaseTime", [days * 86400]);
+  ethers.provider.send("evm_mine", []);
+}
+
 describe('CaminoHub', () => {
   let mugen: SignerWithAddress;
   let jin: SignerWithAddress;
@@ -54,6 +59,71 @@ describe('CaminoHub', () => {
 
     });
 
+    it('Should be able to launch a new airdrop', async () => {
+      await token.transfer(fuu.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(fuu).approve(caminoHub.address, ethers.utils.parseUnits("100", 18));
+      await expect(caminoHub.connect(fuu).launchAirdrop(token.address, 1000, 200000, ethers.utils.parseEther("30"))).to.emit(airdropHandler, 'AirdropLaunched');
+    });
+
+    it('Should be able to stake', async () => {
+      await token.transfer(jin.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(jin).approve(stakingHandler.address, ethers.utils.parseUnits("100", 18));
+      await expect(stakingHandler.connect(jin).stake(ethers.utils.parseUnits("100", 18))).to.emit(stakingHandler, 'Staked');
+    });
+
+    it('Should be able to unstake', async () => {
+      await token.transfer(jin.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(jin).approve(stakingHandler.address, ethers.utils.parseUnits("100", 18));
+      await stakingHandler.connect(jin).stake(ethers.utils.parseUnits("100", 18));
+      await expect(stakingHandler.connect(jin).unstake(ethers.utils.parseUnits("100", 18))).to.emit(stakingHandler, 'Unstaked');
+    })
+
+    it('Should be able to claim rewards', async () => {
+      await token.transfer(jin.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(jin).approve(stakingHandler.address, ethers.utils.parseUnits("100", 18));
+      await stakingHandler.connect(jin).stake(ethers.utils.parseUnits("100", 18));
+      await skipDays(1000);
+      await expect(stakingHandler.connect(jin).claimRewards()).to.emit(stakingHandler, 'RewardsClaimed');
+    })
+
+    it('Should be able to claim airdrop', async () => {
+      await token.transfer(fuu.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(fuu).approve(airdropHandler.address, ethers.utils.parseUnits("100", 18));
+      await airdropHandler.connect(fuu).claimAirdrop();
+      await expect(airdropHandler.connect(fuu).claimAirdrop()).to.emit(airdropHandler, 'AirdropClaimed');
+    });
+
+    it("Should not be able to launch a new staking if the staking is already launched", async () => {
+      const duration = 1000;
+
+      await token.transfer(jin.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(jin).approve(caminoHub.address, ethers.utils.parseUnits("100", 18));
+      await expect(caminoHub.connect(jin).launchStaking(token.address, 1000, 200000, ethers.utils.parseEther("30"), duration)).to.be.revertedWith("Staking already launched");
+    })
+
+    it("Should not be able to launch a new airdrop if the airdrop is already launched", async () => {
+      await token.transfer(fuu.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(fuu).approve(caminoHub.address, ethers.utils.parseUnits("100", 18));
+      await expect(caminoHub.connect(fuu).launchAirdrop(token.address, 1000, 200000, ethers.utils.parseEther("30"))).to.be.revertedWith("Airdrop already launched");
+    })
+
+    it("Should not be able to stake if the staking is not launched", async () => {
+      await token.transfer(jin.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(jin).approve(stakingHandler.address, ethers.utils.parseUnits("100", 18));
+      await expect(stakingHandler.connect(jin).stake(ethers.utils.parseUnits("100", 18))).to.be.revertedWith("Staking not launched");
+    })
+
+    it("Should not be able to unstake if the staking is not launched", async () => {
+      await token.transfer(jin.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(jin).approve(stakingHandler.address, ethers.utils.parseUnits("100", 18));
+      await expect(stakingHandler.connect(jin).unstake(ethers.utils.parseUnits("100", 18))).to.be.revertedWith("Staking not launched");
+    });
+
+    it("Should not allow user to claim Airdrop if he hasn't staked", async () => {
+      await token.transfer(fuu.address, ethers.utils.parseUnits("100", 18));
+      await token.connect(fuu).approve(airdropHandler.address, ethers.utils.parseUnits("100", 18));
+      await expect(airdropHandler.connect(fuu).claimAirdrop()).to.be.revertedWith("You haven't staked yet");
+    });
   })
 
 });
